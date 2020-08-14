840a6e79fdb1bd4e42a4ccd7e1ddaf118de1bec8
marlon20m <marlonmora.ndr@gmail.com>
2020-08-13 19:38:33 -0500

    adding folder organization

--- client/assets/script/script.js
+++
@@ -1,8 +0,0 @@
-$("#about").click(function () {
-  footer.scrollIntoView();
-});
-
-document.addEventListener("DOMContentLoaded", function () {
-  const gallery = document.querySelectorAll(".materialboxed");
-  M.Materialboxed.init(gallery, {});
-});
